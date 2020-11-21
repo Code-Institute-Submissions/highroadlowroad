f96df87667a122400c1f734fcdceb36d195b6df7
LHBank <wmbanksph@gmail.com>
2020-11-21 17:00:04 +0000

    Implemented open source Scroll reveal from Github pages and Javascript relevant to this project. Added Footer button styling to animate.css.

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -11,9 +11,89 @@ for(let j=0; j<logoTwo.length; j++){
   console.log(`Letter ${j} is ${logoTwo[j].getTotalLength()}`);
 }
 
+//Scroll Reveal
+window.sr = ScrollReveal();
+  sr.reveal('.tlContain',{
+    duration: 2500,
+    origin:'bottom',
+    distance: '1000px'
+  });
+  sr.reveal('.TagText',{
+    delay: 10,
+    duration: 2500,
+    origin:'bottom',
+    distance: '300px'
+  });
+  sr.reveal('.sectionHeading',{
+    delay: 10,
+    duration: 2500,
+    origin:'bottom',
+    distance: '300px'
+  });
+  sr.reveal('.sectionSubHead',{
+    delay: 10,
+    duration: 2500,
+    origin:'bottom',
+    distance: '300px'
+  });
+  sr.reveal('.p1',{
+    delay: 10,
+    duration: 2500,
+    origin:'bottom',
+    distance: '300px'
+  });
+  sr.reveal('.p2',{
+    delay: 10,
+    duration: 2500,
+    origin:'bottom',
+    distance: '300px'
+  });
+  sr.reveal('.p3',{
+    delay: 10,
+    duration: 2500,
+    origin:'bottom',
+    distance: '300px'
+  });
+  sr.reveal('.mapLegend',{
+    delay: 10,
+    duration: 2500,
+    origin:'bottom',
+    distance: '300px'
+  });
+  sr.reveal('.card1',{
+    delay: 10,
+    duration: 2500,
+    origin:'top',
+    distance: '300px'
+  });
+  sr.reveal('.card2',{
+    delay: 10,
+    duration: 2500,
+    origin:'bottom',
+    distance: '300px'
+  });
+  sr.reveal('#map',{
+    delay: 10,
+    duration: 2500,
+    origin:'bottom',
+    distance: '300px'
+  });
+  sr.reveal('.myForm',{
+    delay: 10,
+    duration: 2500,
+    origin:'bottom',
+    distance: '300px'
+  });  
+  sr.reveal('.cDetails',{
+    delay: 10,
+    duration: 2500,
+    origin:'bottom',
+    distance: '300px'
+  });
+
 //Section Image Change
 
-document.getElementById("footerBtn").addEventListener("click",function(){
+  document.getElementById("footerBtn").addEventListener("click",function(){
     [...document.querySelectorAll(".bgImage")]
       .forEach(img => img.classList.toggle("checked",this.checked))
-})
+  })
