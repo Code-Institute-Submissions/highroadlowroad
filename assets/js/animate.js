7c42271bcd19a9e766b9b88ae5dfeddfd4b93319
LHBank <wmbanksph@gmail.com>
2020-11-21 16:45:16 +0000

    Implemented Contact and Footer elements with respective CSS. JS implemented for image section change.

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -10,3 +10,10 @@ const logoTwo = document.querySelectorAll('#LRlogo path');
 for(let j=0; j<logoTwo.length; j++){
   console.log(`Letter ${j} is ${logoTwo[j].getTotalLength()}`);
 }
+
+//Section Image Change
+
+document.getElementById("footerBtn").addEventListener("click",function(){
+    [...document.querySelectorAll(".bgImage")]
+      .forEach(img => img.classList.toggle("checked",this.checked))
+})
